0cc938a39779a10942ec20b98e2fdc6d6b548b23
heinwol <heinvvol@gmail.com>
2023-11-08 18:51:40 +0300

    switched poetry python version to 3.10

--- flake.nix
+++ flake.nix
@@ -15,16 +15,26 @@
       let
         # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
         pkgs = nixpkgs.legacyPackages.${system};
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+        # inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+        poetry2nixLib = (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; });
+
+        devEnv = poetry2nixLib.mkPoetryEnv {
+          projectDir = self;
+          python = pkgs.python310;
+        };
       in
       {
         packages = {
-          myapp = mkPoetryApplication { projectDir = self; };
+          myapp = poetry2nixLib.mkPoetryApplication { projectDir = self; };
           default = self.packages.${system}.myapp;
         };
 
         devShells.default = pkgs.mkShell {
-          packages = [ pkgs.poetry ];
+          packages = [
+            (pkgs.poetry.override { python3 = pkgs.python310; })
+            # devEnv
+            (pkgs.python310)
+          ];
         };
       });
 }
